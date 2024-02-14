ae3708e7a7eaeaef0a19ae82d9b0a94a99342ecd
Leonardo <leonardosantos2742@gmail.com>
2024-02-14 12:03:54 -0300

    feat: criando repository de tarefa e começando a criar controllers

--- GerenciadorTarefas.Domain/Entities/Tarefa.cs
+++ GerenciadorTarefas.Domain/Entities/Tarefa.cs
@@ -1,20 +1,36 @@
 ﻿using GerenciadorTarefas.Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace GerenciadorTarefas.Domain.Entities
 {
     public class Tarefa
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("titulo")]
         public string Titulo { get; set; }
+
+        [JsonPropertyName("descricao")]
         public string Descricao { get; set; }
+
+        [JsonPropertyName("data_criacao")]
         public DateTime DataCriacao { get; set; }
+
+        [JsonPropertyName("data_vencimento")]
         public DateTime DataVencimento { get; set; }
+
+        [JsonPropertyName("status")]
         public Status Status { get; set; }
+
+        [JsonPropertyName("prioridade")]
         public Prioridade Prioridade { get; set; }
 
 
         //Relacionamento
+        [JsonPropertyName("projeto")]
         public Projeto Projeto { get; set; }
+        [JsonPropertyName("projetoid")]
         public int ProjetoId { get; set; }
 
         public Tarefa() { }
